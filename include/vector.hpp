1f54fd7c34e7207959290b9668ed1be459b1eda8
X4Ker <32191102+X4Ker@users.noreply.github.com>
2018-04-17 22:46:35 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cassert>
 #include <iostream>
 
 class vector_t
@@ -25,3 +27,125 @@ public:
 };
 
 bool operator !=(vector_t const & lhs, vector_t const & rhs);
+
+vector_t::vector_t()
+{
+	elements_ = nullptr;
+	size_ = 0;
+	capacity_ = 0;
+}
+
+vector_t::vector_t(vector_t const & other)
+{
+	size_ = other.size_;
+	capacity_ = other.capacity_;
+
+	elements_ = new int[capacity_];
+	for (std::size_t i = 0; i < size_; ++i) {
+		elements_[i] = other.elements_[i];
+	}
+}
+
+vector_t & vector_t::operator =(vector_t const & other)
+{
+	if (this != &other) {
+		delete[] elements_;
+	}
+
+	size_ = other.size_;
+	capacity_ = other.capacity_;
+
+	elements_ = new int[capacity_];
+	for (std::size_t i = 0; i < size_; ++i) {
+		elements_[i] = other.elements_[i];
+	}
+
+	return *this;
+}
+
+bool vector_t::operator ==(vector_t const & other) const
+{
+	bool success = false;
+	if (size_ == other.size_ && capacity_ == other.capacity_) {
+		for (size_t i = 0; i < size_; i++) {
+			if (elements_[i] == other.elements_[i]) {
+				success = true;
+				break;
+			}
+			else {
+				success = false;
+			}
+		}
+	}
+	if (success) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+vector_t::~vector_t()
+{
+	delete[] elements_;
+}
+
+std::size_t vector_t::size() const
+{
+	return size_;
+}
+
+std::size_t vector_t::capacity() const
+{
+	return capacity_;
+}
+
+void vector_t::push_back(int value)
+{
+	if (size_ == capacity_) {
+		if (capacity_ == 0) capacity_ = 1;
+		else capacity_ *= 2;
+		T * p = new T[capacity_];
+		for (std::size_t i = 0; i<size_; i++) {
+			p[i] = elements_[i];
+		}
+		delete[] elements_;
+		elements_ = p;
+	}
+
+	elements_[size_++] = value;
+}
+
+void vector_t::pop_back()
+{
+	if (size_ == 0) return;
+	size_--;
+	if (size_ == capacity_ / 4) {
+		capacity_ /= 2;
+		T * p = new T[capacity_];
+		for (std::size_t i = 0; i<size_; i++) {
+			p[i] = elements_[i];
+		}
+		delete[] elements_;
+		elements_ = p;
+	}
+}
+
+int & vector_t::operator [](std::size_t index)
+{
+	return elements_[index];
+}
+
+int vector_t::operator [](std::size_t index) const
+{
+	return elements_[index];
+}
+
+bool operator !=(vector_t const & lhs, vector_t const & rhs)
+{
+	bool success = true;
+	if (lhs == rhs) {
+		return !success;
+	}
+	return success;
+}
