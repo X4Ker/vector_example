2f498795f3e93d631c25be204c870538b2ec802e
X4Ker <32191102+X4Ker@users.noreply.github.com>
2018-05-07 01:58:27 +0300

    Update vector.hpp

--- include/vector.hpp
+++ include/vector.hpp
@@ -2,51 +2,56 @@
 #include <cassert>
 #include <iostream>
 
+template <typename T>
 class vector_t
 {
 private:
-	int * elements_;
+	T * elements_;
 	std::size_t size_;
 	std::size_t capacity_;
 public:
 	vector_t();
-	vector_t(vector_t const & other);
-	vector_t & operator =(vector_t const & other);
+	vector_t(vector_t<T> const & other);
+	vector_t<T> & operator =(vector_t<T> const & other);
 	~vector_t();
 
 	std::size_t size() const;
 	std::size_t capacity() const;
 
-	void push_back(int value);
+	void push_back(T value);
 	void pop_back();
 
-	int & operator [](std::size_t index);
-	int operator [](std::size_t index) const;
+	T & operator [](std::size_t index);
+	T operator [](std::size_t index) const;
+	T & at (std::size_t index);
 
 	bool operator ==(vector_t const & other) const;
 };
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs);
+bool operator !=(vector_t<T> const & lhs, vector_t<T> const & rhs);
 
-vector_t::vector_t()
+template <typename T>
+vector_t<T>::vector_t()
 {
 	elements_ = nullptr;
 	size_ = 0;
 	capacity_ = 0;
 }
 
-vector_t::vector_t(vector_t const & other)
+template <typename T>
+vector_t<T>::vector_t(vector_t<T> const & other)
 {
 	size_ = other.size_;
 	capacity_ = other.capacity_;
 
-	elements_ = new int[capacity_];
+	elements_ = new T[capacity_];
 	for (std::size_t i = 0; i < size_; ++i) {
 		elements_[i] = other.elements_[i];
 	}
 }
 
-vector_t & vector_t::operator =(vector_t const & other)
+template <typename T>
+vector_t<T> & vector_t<T>::operator =(vector_t<T> const & other)
 {
 	if (this != &other) {
 		delete[] elements_;
@@ -54,7 +59,7 @@ vector_t & vector_t::operator =(vector_t const & other)
 	size_ = other.size_;
 	capacity_ = other.capacity_;
 
-	elements_ = new int[capacity_];
+	elements_ = new T[capacity_];
 	for (std::size_t i = 0; i < size_; ++i) {
 		elements_[i] = other.elements_[i];
 	}
@@ -63,7 +68,8 @@ vector_t & vector_t::operator =(vector_t const & other)
 	return *this;
 }
 
-bool vector_t::operator ==(vector_t const & other) const
+template <typename T>
+bool vector_t<T>::operator ==(vector_t<T> const & other) const
 {
 	bool success = false;
 	if (size_ == other.size_ && capacity_ == other.capacity_) {
@@ -85,27 +91,31 @@ bool vector_t::operator ==(vector_t const & other) const
 	}
 }
 
-vector_t::~vector_t()
+template <typename T>
+vector_t<T>::~vector_t()
 {
 	delete[] elements_;
 }
 
-std::size_t vector_t::size() const
+template <typename T>
+std::size_t vector_t<T>::size() const
 {
 	return size_;
 }
 
-std::size_t vector_t::capacity() const
+template <typename T>
+std::size_t vector_t<T>::capacity() const
 {
 	return capacity_;
 }
 
-void vector_t::push_back(int value)
+template <typename T>
+void vector_t<T>::push_back(T value)
 {
 	if (size_ == capacity_) {
 		if (capacity_ == 0) capacity_ = 1;
 		else capacity_ *= 2;
-		int * p = new int[capacity_];
+		int * p = new T[capacity_];
 		for (std::size_t i = 0; i<size_; i++) {
 			p[i] = elements_[i];
 		}
@@ -116,13 +126,14 @@ void vector_t::push_back(int value)
 	elements_[size_++] = value;
 }
 
-void vector_t::pop_back()
+template <typename T>
+void vector_t<T>::pop_back()
 {
 	if (size_ == 0) return;
 	size_--;
 	if (size_ == capacity_ / 4) {
 		capacity_ /= 2;
-		int * p = new int[capacity_];
+		T * p = new T[capacity_];
 		for (std::size_t i = 0; i<size_; i++) {
 			p[i] = elements_[i];
 		}
@@ -131,17 +142,29 @@ void vector_t::pop_back()
 	}
 }
 
-int & vector_t::operator [](std::size_t index)
+template <typename T>
+T & vector_t<T>::operator [](std::size_t index)
 {
 	return elements_[index];
 }
 
-int vector_t::operator [](std::size_t index) const
+template <typename T>
+T vector_t<T>::operator [](std::size_t index) const
 {
 	return elements_[index];
 }
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs)
+template <typename T>
+T & vector_t<T>::at (std::size_t index)
+{
+	if ( index >= size_ ) {
+		throw std::out_of_range( "Error range" );
+	}
+	return elements_[index];
+}
+
+template <typename T>
+bool operator !=(vector_t<T> const & lhs, vector_t<T> const & rhs)
 {
 	bool success = true;
 	if (lhs == rhs) {
